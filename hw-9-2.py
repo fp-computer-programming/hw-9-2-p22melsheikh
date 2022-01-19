918f29cacc86da57ec993fbde18987d1a7fbe710
x <xxx@tsststs.com>
2022-01-19 01:04:36 -0500

    hw

---
+++ hw-9-2.py
@@ -0,0 +1,13 @@
+# MEE 1/18/22
+
+
+lastname_initials = ["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N", "N", "O", "P", "P", "X", "T", "S", "S", "P"]
+
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin", "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+
+index = 0
+
+for row in rows:
+    for name in (row):
+        print("{0} {1}".format(name, lastname_initials[index]))
+        index += 1
